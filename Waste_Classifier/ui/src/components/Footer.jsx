790e147c2000a4fdc6609d102e2f3cca4d6962d1
Adrian Rodel Bangalando <bangalandoam@students.national-u.edu.ph>
2025-10-09 11:33:47 +0800

    Delete Waste_Classifier/ui directory

--- Waste_Classifier/ui/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-20 py-6 text-center text-gray-500 border-t border-white/10">
-      <p>© {new Date().getFullYear()} Waste Classifier. All rights reserved.</p>
-    </footer>
-  );
-}
